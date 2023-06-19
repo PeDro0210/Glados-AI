aa92c321956619cea15657de1ba0018f8a0fab2f
Pedro Avila <123851480+PeDro0210@users.noreply.github.com>
2023-06-18 19:09:20 -0600

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import setup, Executable
-import sys
-
-sys.setrecursionlimit(3000)
-
-setup(name='GLaDOS_AI',
-      version='0.1',
-      executables = [Executable("glados_AI_main.py",icon="Logo.ico")])
-
